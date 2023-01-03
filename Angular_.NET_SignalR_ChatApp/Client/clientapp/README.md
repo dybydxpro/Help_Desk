ebb27e96173a575c7aa887e024558d928fa5c20f
tharindu-evoke <tharindu@evoke.lk>
2023-01-03 15:06:15 +0530

    Commit Angular project

--- Angular_.NET_SignalR_ChatApp/Client/clientapp/README.md
+++ Angular_.NET_SignalR_ChatApp/Client/clientapp/README.md
@@ -1,4 +1,4 @@
-# Chatapp
+# Clientapp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.4.
 
--- Angular_.NET_SignalR_ChatApp/Client/clientapp/src/app/app.component.spec.ts
+++ Angular_.NET_SignalR_ChatApp/Client/clientapp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'chatapp'`, () => {
+  it(`should have as title 'clientapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('chatapp');
+    expect(app.title).toEqual('clientapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('chatapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('clientapp app is running!');
   });
 });
